003909e2a45bb7a3d65aad7d3e4b1c1fab9dec0b
Jason Priem <jp@jasonpriem.org>
2013-01-21 21:56:29 -0500

    show test text.

--- silsannotate/views.py
+++ silsannotate/views.py
@@ -1,5 +1,15 @@
+from flask import render_template, abort
+from jinja2 import TemplateNotFound
 from silsannotate import app
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/text/<text_id>')
+def show_text(text_id):
+    try:
+        return render_template(text_id)
+    except TemplateNotFound:
+        abort(404, "Whoops, we can't find that...maybe you typed the name wrong?")
