3bda80d885fe5549d1c0719f1de1b864b383a0c2
Jason Priem <jp@jasonpriem.org>
2013-01-23 14:23:50 -0500

    pushes new annotatoins to server, gets 'em again on pageload.

--- silsannotate/static/js/silsannotate.js
+++ silsannotate/static/js/silsannotate.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-    console.log("i ran");
-    $(document.body).annotator();
+    var textId = window.location.pathname.split("/")[2]
+    var content = $(document.body).annotator();
+    content.annotator('addPlugin', 'Store', {
+        // The endpoint of the store on your server.
+        prefix: '/api',
+
+        // Attach the uri of the current page to all annotations to allow search.
+        annotationData: {
+            'textId': textId
+        }
+
+        // This will perform a "search" action rather than "read" when the plugin
+        // loads. Will request the last 20 annotations for the current url.
+        // eg. /store/endpoint/search?limit=20&uri=http://this/document/only
+        ,loadFromSearch: {
+            'limit': 20,
+            'textId':textId
+        }
+    });
 
 })
--- silsannotate/views.py
+++ silsannotate/views.py
@@ -1,10 +1,13 @@
-from flask import render_template, abort
+import json, couchdb, os, shortuuid
+
+from flask import render_template, abort, request, make_response
 from jinja2 import TemplateNotFound
 from silsannotate import app
 from flask.ext.assets import Environment, Bundle
 
-assets = Environment(app)
 
+couch = couchdb.Server(url=os.getenv("SILS_CLOUDANT_URL"))
+db = couch[os.getenv("SILS_CLOUDANT_DB")]
 
 @app.route('/')
 def hello_world():
@@ -13,6 +16,41 @@ def hello_world():
 @app.route('/text/<text_id>')
 def show_text(text_id):
     try:
-        return render_template(text_id)
+        return render_template(text_id+".html")
     except TemplateNotFound:
         abort(404, "Whoops, we can't find that...maybe you typed the name wrong?")
+
+
+@app.route("/store")
+def store_root():
+    pass
+
+@app.route("/api/search")
+def search():
+    textId = request.args.get("textId")
+    view = db.view("main/by_textId")
+
+    matches = view[textId]
+    ret = {
+        "total": matches.total_rows,
+        "rows": []
+    }
+
+    for anno in matches.rows:
+        doc = anno["value"]
+        doc["id"] = doc["_id"]
+        ret["rows"].append(doc)
+
+    resp = make_response(json.dumps(ret, indent=4), 200)
+    resp.mimetype = "application/json"
+    return resp
+
+@app.route("/api/annotations", methods=["POST"])
+def post_new_annotation():
+    doc = request.json
+    doc["_id"] = shortuuid.uuid()
+    couch_resp = db.save(doc)
+
+    resp = make_response(json.dumps(couch_resp, indent=4), 200)
+    resp.mimetype = "application/json"
+    return resp
