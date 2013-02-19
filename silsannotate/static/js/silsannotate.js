86151b5ae782dad2de75be5f5fd116d9cdf4f8df
Jason Priem <jp@jasonpriem.org>
2013-02-19 01:39:41 -0500

    Add userId from url to annotation.

--- silsannotate/static/js/silsannotate.js
+++ silsannotate/static/js/silsannotate.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 
 
     var textId = window.location.pathname.split("/")[2]
+    var userId = window.location.href.match(/user=(\w+)/)[1]
+
     var content = $(document.body).annotator();
     content.annotator('addPlugin', 'Store', {
         // The endpoint of the store on your server.
@@ -12,7 +14,8 @@ $(document).ready(function(){
 
         // Attach the uri of the current page to all annotations to allow search.
         annotationData: {
-            'textId': textId
+            'textId': textId,
+            'userId': userId
         }
 
         // This will perform a "search" action rather than "read" when the plugin
